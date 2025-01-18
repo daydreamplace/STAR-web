c86039c6e66aa6befb388409b64914b74b16248b
Eden <you8inpark@gmail.com>
2025-01-18 23:38:22 +0900

    design: 유성효과 z-index 설정

--- src/components/common/MeteorEffect.tsx
+++ src/components/common/MeteorEffect.tsx
@@ -1,5 +1,66 @@
 import { useEffect, useState } from "react";
-import { createGlobalStyle, keyframes } from "styled-components";
+import styled, { createGlobalStyle, keyframes } from "styled-components";
+
+interface MeteorEffectProps {
+  count?: number;
+  maxDelay?: number;
+  minSpeed?: number;
+  maxSpeed?: number;
+  angle?: number;
+  direction?: "left" | "right";
+}
+
+const MAX_STAR_COUNT = 50;
+const colors = ["#c77eff", "#f6ff7e", "#ff8d7e", "#ffffff"];
+
+const MeteorEffect = ({
+  count = 12,
+  maxDelay = 15,
+  minSpeed = 2,
+  maxSpeed = 4,
+  angle = 30,
+  direction = "right",
+}: MeteorEffectProps) => {
+  const starCount = count < MAX_STAR_COUNT ? count : MAX_STAR_COUNT;
+  const [starInterval, setStarInterval] = useState<number>(0);
+
+  useEffect(() => {
+    const calcStarInterval = () => {
+      const innerWidth = window.innerWidth;
+      setStarInterval(Math.floor((innerWidth * 1.5) / (count * 5)));
+    };
+    calcStarInterval();
+    window.addEventListener("resize", calcStarInterval);
+    return () => {
+      window.removeEventListener("resize", calcStarInterval);
+    };
+  }, [count]);
+
+  return (
+    <Wrapper>
+      <GlobalMeteorStyle $direction={direction} $angle={angle} />
+      {new Array(starCount).fill(0).map((_, idx) => {
+        const left =
+          direction === "left"
+            ? `${Math.random() * count * 5 * starInterval}px`
+            : `${window.innerHeight - Math.random() * count * 5 * starInterval}px`;
+        const animationDelay = `${Math.random() * maxDelay}s`;
+        const animationDuration =
+          maxSpeed > minSpeed ? `${minSpeed + Math.random() * maxSpeed}s` : `${2 + Math.random() * 4}`;
+        const colorIndex = Math.floor(Math.random() * colors.length - 0.001);
+        const size = `${2 + Math.floor(Math.random() * 5)}px`;
+        const boxShadow = `0px 0px 10px 3px ${colors[colorIndex]}`;
+        return (
+          <div
+            key={idx}
+            style={{ left, animationDelay, animationDuration, boxShadow, width: size, height: size }}
+            className="star"
+          ></div>
+        );
+      })}
+    </Wrapper>
+  );
+};
 
 const MeteorKeyframe = (direction: "left" | "right", angle: number) => keyframes`
   0% {
@@ -46,67 +107,14 @@ const GlobalMeteorStyle = createGlobalStyle<GlobalMeteorProps>`
   }
 `;
 
-interface MeteorEffectProps {
-  count?: number;
-  white?: boolean;
-  maxDelay?: number;
-  minSpeed?: number;
-  maxSpeed?: number;
-  angle?: number;
-  direction?: "left" | "right";
-}
-
-const MAX_STAR_COUNT = 50;
-const colors = ["#c77eff", "#f6ff7e", "#ff8d7e", "#ffffff"];
-
-const MeteorEffect = ({
-  count = 12,
-  //   white = false,
-  maxDelay = 15,
-  minSpeed = 2,
-  maxSpeed = 4,
-  angle = 30,
-  direction = "right",
-}: MeteorEffectProps) => {
-  const starCount = count < MAX_STAR_COUNT ? count : MAX_STAR_COUNT;
-  const [starInterval, setStarInterval] = useState<number>(0);
-
-  useEffect(() => {
-    const calcStarInterval = () => {
-      const innerWidth = window.innerWidth;
-      setStarInterval(Math.floor((innerWidth * 1.5) / (count * 5)));
-    };
-    calcStarInterval();
-    window.addEventListener("resize", calcStarInterval);
-    return () => {
-      window.removeEventListener("resize", calcStarInterval);
-    };
-  }, [count]);
-
-  return (
-    <>
-      <GlobalMeteorStyle $direction={direction} $angle={angle} />
-      {new Array(starCount).fill(0).map((_, idx) => {
-        const left =
-          direction === "left"
-            ? `${Math.random() * count * 5 * starInterval}px`
-            : `${window.innerHeight - Math.random() * count * 5 * starInterval}px`;
-        const animationDelay = `${Math.random() * maxDelay}s`;
-        const animationDuration =
-          maxSpeed > minSpeed ? `${minSpeed + Math.random() * maxSpeed}s` : `${2 + Math.random() * 4}`;
-        const colorIndex = Math.floor(Math.random() * colors.length - 0.001); // 별 색상
-        const size = `${2 + Math.floor(Math.random() * 5)}px`; // 별 크기
-        const boxShadow = `0px 0px 10px 3px ${colors[colorIndex]}`;
-        return (
-          <div
-            key={idx}
-            style={{ left, animationDelay, animationDuration, boxShadow, width: size, height: size }}
-            className="star"
-          ></div>
-        );
-      })}
-    </>
-  );
-};
+const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -10;
+  pointer-events: none;
+`;
 
 export default MeteorEffect;
