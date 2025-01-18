c0cbfb0f60d333b7a4d02c5edde0d25e647013fa
Eden <you8inpark@gmail.com>
2025-01-18 22:00:16 +0900

    feat: 백 그라운드 유성 이펙트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,19 @@ import Home from "./pages/Home";
 import Footer from "./components/layout/Footer";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
+import MeteorEffect from "./components/common/MeteorEffect";
 import theme from "./styles/Theme";
 
 function App() {
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <MeteorEffect count={20} angle={45} direction="right" />
       <Header />
       <Home />
       <Footer />
     </ThemeProvider>
-  )
+  );
 }
 
 export default App;
