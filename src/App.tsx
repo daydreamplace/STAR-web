581879fefd70500fcb3e5a24cdc8b3b29dbbfea8
Eden <you8inpark@gmail.com>
2025-02-14 17:46:34 +0900

    refactor: BrowserRouter를 HashRouter로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Home from "./pages/Home";
 import Vision from "./pages/Vision";
